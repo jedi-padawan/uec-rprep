b2a38f39ddd86e135706ca860c9e1f3e733ba6e8
jedi-padawan <72233108+jedi-padawan@users.noreply.github.com>
2022-06-03 05:09:46 +0900

    Adding two functions.

--- library.r
+++ library.r
@@ -20,3 +20,47 @@ require.with.install.packages.if.necesarry = function(packages) {
     require.2(packages)    
   }
 }
+
+detach.all.extra.packages <- function() {
+  packages.default <- getOption("defaultPackages")
+  search.paths <- search()
+  packages.loaded <- search.paths[ifelse(unlist(gregexpr("package:", 
+                                                         search.paths)) == 1, 
+                                         TRUE, 
+                                         FALSE)]
+  packages.extra <- setdiff(packages, packages.default)
+  lapply(packages.extra, detach, character.only = TRUE)
+}
+
+set.proxy = function(proxy) {
+  if( is.list(proxy) ) {
+    if(length(setdiff(names(proxy), c("http", "https", "ftp"))) == 0 ) {
+      Sys.setenv("http_proxy"=proxy$http)
+      Sys.setenv("https_proxy"=proxy$https)
+      Sys.setenv("ftp_proxy"=proxy$ftp)
+      return(TRUE)
+    } else {
+      return(FALSE)
+    }
+  } else if (is.vector(proxy)) {
+    if( length(proxy) == 3 ) {
+      Sys.setenv("http_proxy"=proxy[1])
+      Sys.setenv("https_proxy"=proxy[2])
+      Sys.setenv("ftp_proxy"=proxy[3])
+      return(TRUE)
+    } else if (is.character(proxy[1])) {
+      Sys.setenv("http_proxy"=proxy[1])
+      Sys.setenv("https_proxy"=proxy[1])
+      Sys.setenv("ftp_proxy"=proxy[1])
+      return(TRUE)
+    } else {
+      return(FALSE)
+    }
+  } else if (is.character(proxy)) {
+    Sys.setenv("http_proxy"=proxy)
+    Sys.setenv("https_proxy"=proxy)
+    Sys.setenv("ftp_proxy"=proxy)
+    return(TRUE)
+  }
+  return(FALSE)
+}
